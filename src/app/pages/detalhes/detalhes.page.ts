66c5197d184475a3878c4c17ce262ed66d902305
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-06-22 13:04:33 -0300

    :sparkles: feat: set components as standalone for detalhes, favoritos, and home pages

--- src/app/pages/detalhes/detalhes.page.ts
+++ src/app/pages/detalhes/detalhes.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-detalhes',
   templateUrl: './detalhes.page.html',
   styleUrls: ['./detalhes.page.scss'],
--- src/app/pages/favoritos/favoritos.page.ts
+++ src/app/pages/favoritos/favoritos.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-favoritos',
   templateUrl: './favoritos.page.html',
   styleUrls: ['./favoritos.page.scss'],
